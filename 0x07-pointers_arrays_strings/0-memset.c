e7ed6a0a2f2819225ca174519c43e6f57b61a4c0
Ghizo2001 <gabi.66ok@gmail.com>
2023-04-03 09:14:41 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Fill a block of memory with a specified byte value
+ * @s: pointer to the memory area to be filled
+ * @b: the byte value to be copied
+ * @n: number of bytes to be filled
+ *
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
