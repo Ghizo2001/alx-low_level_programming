14c5ed92e4ad65405049e8294055a78f3a9cfdcf
Ghizo2001 <gabi.66ok@gmail.com>
2023-04-03 14:52:50 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - Finds the first occurrence of any character in `accept` within `s`
+ * @s: The string to be searched
+ * @accept: The set of characters to search for
+ *
+ * Return: Always 0(Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int g;
+
+	while (*s)
+	{
+		for (g = 0; accept[g]; g++)
+		{
+			if (*s == accept[g])
+				return (s);
+		}
+			s++;
+	}
+	return ('\0');
+}
