2e4d886a37d1fe2f8d3b56bb2ac8f5177913b262
Ghizo2001 <gabi.66ok@gmail.com>
2023-04-09 16:36:35 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int num_denominations = 5;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (int i = 0; i < num_denominations; i++)
+	{
+		while (cents >= denominations[i])
+		{
+			coins++;
+			cents -= denominations[i];
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
